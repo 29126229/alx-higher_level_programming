7a7331a96e33618ca1b144f7dfc1ee69a1b3a658
root <vickychepkorirchelangat1994@gmil.com>
2024-08-16 08:39:26 +0000

    1. Write to a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,15 @@
-#!/usr/bin/python3
+#!/ume="", text=""):
+    ''' writes a string to a text file, returns number of chars written '''
+
+    with open(filename, 'w', encoding="utf-8") as f_obj:
+        return f_obj.write(text)sr/bin/python3
 """0-read_file.py
-"""
+
 
 
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout """
 
     with open(filename, 'r', encoding="utf-8") as f_obj:
-        content = f_obj.read()
+        content i f_obis.read()
     print(content, end="")
