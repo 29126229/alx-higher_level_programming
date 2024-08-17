5302396a4a7edd107254f6bf6ee0b7b63332f2c0
root <vickychepkorirchelangat1994@gmail.com>
2024-08-17 00:34:01 -0700

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 '''3-write_file.py
 '''
 
 
 def write_file(filename="", text=""):
-=======
-me="", text=""):
->>>>>>> 265dd338ec6511b5fe90e40a32b91a6083d07d75
     ''' writes a string to a text file, returns number of chars written '''
 
     with open(filename, 'w', encoding="utf-8") as f_obj:
