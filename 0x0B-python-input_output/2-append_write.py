552b1708d65785725ebc3f2bc5f183533e03c488
VICKY <133225622+29126229@users.noreply.github.com>
2024-08-17 00:21:03 -0700

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,9 +0,0 @@
-ppend_write.py
-'''
-
-
-def append_write(filename="", text=""):
-    ''' appends a string at the end of a text file '''
-
-    with open(filename, 'a', encoding="utf-8") as f_obj:
-        return f_obj.write(text)
