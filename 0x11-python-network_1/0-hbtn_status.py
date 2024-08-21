cc256926c0e9ee791b2005b28cc93d39b78bbda2
root <vickychepkorirchelangat1994@gmail.com>
2024-08-21 10:23:57 -0700

    What's my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
-import urllib.request
-
+'''
+A Python script that fetches https://alx-intranet.hbtn.io/status
+'''
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        r = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(r)))
-        print("\t- content: {}".format(r))
-        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
+    import urllib.request
+
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as resp:
+        content = resp.read()
+
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
